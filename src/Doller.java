bb7f260648a3739d84ea20c93e210ccf8c70d43f
okuzawats <okuzawats@gmail.com>
2020-12-30 08:32:39 +0900

    implement chapter 3

--- src/Doller.java
+++ src/Doller.java
@@ -1,9 +1,16 @@
 public class Doller {
   int amount;
+
   Doller(int amount) {
     this.amount = amount;
   }
+
   Doller times(int multiplier) {
     return new Doller(amount * multiplier);
+  }
+
+  @Override public boolean equals(Object object) {
+    final Doller doller = (Doller) object;
+    return this.amount == doller.amount;
   }
 }
--- src/MoneyTest.java
+++ src/MoneyTest.java
@@ -8,6 +8,12 @@ public class MoneyTest {
     Doller product = five.times(2);
     assertEquals(10, product.amount);
     product = five.times(3);
-    assertEquals(15, five.amount);
+    assertEquals(15, product.amount);
+  }
+
+  @Test
+  public void testEquality() {
+    assertTrue(new Doller(5).equals(new Doller(5)));
+    assertFalse(new Doller(5).equals(new Doller(6)));
   }
 }
