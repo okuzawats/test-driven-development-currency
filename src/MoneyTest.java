4d8e947fe8e89665c0c16e6cb70abb8c72db4d59
okuzawats <okuzawats@gmail.com>
2020-12-30 21:21:49 +0900

    implement chapter 14

--- src/MoneyTest.java
+++ src/MoneyTest.java
@@ -50,6 +50,19 @@ public class MoneyTest {
   }
 
   @Test
+  public void testReduceMoneyDifferentCurrency() {
+    Bank bank = new Bank();
+    bank.addRate("CHF", "USD", 2);
+    Money result = bank.reduce(Money.franc(2), "USD");
+    assertEquals(Money.dollar(1), result);
+  }
+
+  @Test
+  public void testIdentityRate() {
+    assertEquals(1, new Bank().rate("USD", "USD"));
+  }
+
+  @Test
   public void testEquality() {
     assertTrue(Money.dollar(5).equals(Money.dollar(5)));
     assertFalse(Money.dollar(5).equals(Money.dollar(6)));
